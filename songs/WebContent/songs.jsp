860acd4289649799c42cd4593c645ee9915d42c9
ranjitaxworkz <77092682+ranjitaxworkz@users.noreply.github.com>
2021-06-20 11:21:06 +0530

    Add files via upload

---
+++ songs/WebContent/songs.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body bgcolor="">
+<form action="songs">
+
+     Song Name: <input type="text" name="songName">
+     Featuring: <input type="text" name="featuring">
+     Singer: <input type="text" name="singer">
+     Duration: <input type="text" name="duration">
+     Language: <input type="text" name="language">
+     Lyricist: <input type="text" name="lyricist">
+		<input type="submit" value="submit">
+     </form>
+</body>
+</html>
---
+++ songs/src/com/xworkz/songs/SongsServlet.java
@@ -0,0 +1,46 @@
+package com.xworkz.songs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/songs", initParams = { @WebInitParam(name = "key", value = "Gaana"),
+
+})
+public class SongsServlet extends GenericServlet {
+
+	// public SongsServlet(int n) {
+	// System.out.println(n);
+	// }
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+//		ServletConfig config = getServletConfig();
+//		String name = config.getInitParameter("key");
+//		System.out.println(name);
+
+		String songName = req.getParameter("songName");
+		String featuring = req.getParameter("featuring");
+		String singer = req.getParameter("singer");
+		String duration = req.getParameter("duration");
+		String language = req.getParameter("language");
+		String lyricist = req.getParameter("lyricist");
+
+		PrintWriter printWriter = res.getWriter();
+		printWriter.write("thank you for selecting " + songName);
+	}
+
+}
